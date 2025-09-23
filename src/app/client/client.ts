518fd1af8813be811a2614b7a6e6045147cc50c2
Maria Fernanda Perez Campos <mariafperezcampos@gmail.com>
2025-09-23 00:22:42 -0500

    client-view:changes to the navbar style

--- src/app/client/client.ts
+++ src/app/client/client.ts
@@ -1,40 +1,51 @@
 import { Component, signal, OnInit } from '@angular/core';
-import { Router, RouterOutlet, RouterLink } from '@angular/router';
+import { Router, RouterOutlet, RouterLink, NavigationEnd } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatCardModule } from '@angular/material/card';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatLine } from '@angular/material/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
-import { Observable, map, shareReplay } from 'rxjs';
+import { Observable, filter, map, shareReplay } from 'rxjs';
+import { MATERIAL_IMPORTS } from '../shared/components/material.imports';
 
 @Component({
   selector: 'app-client',
-  imports: [RouterOutlet, RouterLink, CommonModule, MatToolbarModule, MatSidenavModule, MatListModule, MatButtonModule, MatIconModule, MatMenuModule, MatCardModule, MatTableModule, MatPaginatorModule, MatLine],
+  imports: [RouterOutlet, RouterLink, CommonModule, ...MATERIAL_IMPORTS],
   templateUrl: './client.html',
-  styleUrl: './client.css'
+  styleUrl: './client.css',
 })
-export class Client implements OnInit{
-nameUser = signal('');
-isHandset$: Observable<boolean>;
-
-  constructor(private router: Router, private auth: AuthService, private breakpointObserver: BreakpointObserver){
-    this.isHandset$ = this.breakpointObserver.observe([Breakpoints.Handset]).pipe(map(r => r.matches), shareReplay());
+export class Client implements OnInit {
+  private routeTitles: Record<string, string> = {
+    '/client/users': 'Inicio',
+    '/client/accounts': 'Cuentas',
+    '/client/cards': 'Tarjetas',
+    '/client/loans': 'Préstamos',
+    '/client/transfers': 'Transferencias',
+  };
+  nameUser = signal('');
+  isHandset$: Observable<boolean>;
+  currentRoute = signal('');
+  constructor(
+    private router: Router,
+    private auth: AuthService,
+    private breakpointObserver: BreakpointObserver
+  ) {
+    this.isHandset$ = this.breakpointObserver.observe([Breakpoints.Handset]).pipe(
+      map((r) => r.matches),
+      shareReplay()
+    );
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        const newTitle = this.routeTitles[event.urlAfterRedirects] || 'Dashboard Cliente';
+        this.currentRoute.set(newTitle);
+      });
   }
 
   ngOnInit(): void {
-    const name=this.auth.getLoggedInUser()?.name;
+    const name = this.auth.getLoggedInUser()?.name;
     this.nameUser.set(name ? name : 'Admin');
   }
 
-  close(): void{
+  close(): void {
     this.auth.logout();
     this.router.navigate(['/']);
   }
