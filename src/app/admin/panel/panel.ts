a67dc9c0ff042b61fbd80b789d98322b7848ea3e
Itzel Andrea <gv.itzelandrea@gmail.com>
2025-09-22 19:18:20 -0500

    Avance de maquetación admin

--- src/app/admin/panel/panel.ts
+++ src/app/admin/panel/panel.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-panel',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatProgressBarModule,MatIconModule],
   templateUrl: './panel.html',
   styleUrl: './panel.css'
 })
 export class Panel {
+
+  statistics = [
+  {
+    title: 'Clientes',
+    items: [
+      { value: 40, class: 'total', label: 'Clientes actuales' },
+      { value: 40, class: 'new', label: 'Nuevos clientes registrados' },
+      { value: 10, class: 'delete', label: 'Clientes eliminados' }
+    ]
+  },
+  {
+    title: 'Tarjetas',
+    items: [
+      { value: 40, class: 'total', label: 'Tarjetas actuales' },
+      { value: 40, class: 'new', label: 'Solicitudes de nuevas tarjetas' },
+      { value: 10, class: 'delete', label: 'Tarjetas eliminadas/bloqueadas' }
+    ]
+  },
+  {
+    title: 'Cuentas',
+    items: [
+      { value: 40, class: 'total', label: 'Cuentas actuales' },
+      { value: 40, class: 'new', label: 'Nuevos cuentas registrados' },
+      { value: 10, class: 'delete', label: 'Cuentas eliminados' }
+    ]
+  },
+  {
+    title: 'Préstamos',
+    items: [
+      { value: 40, class: 'total', label: 'Préstamos actuales' },
+      { value: 40, class: 'new', label: 'Nuevos Préstamos registrados' },
+      { value: 10, class: 'delete', label: 'Préstamos eliminados' }
+    ]
+  }
+];
+
+  constructor(private router:Router){}
+
+  redirectTo(path: string){
+    this.router.navigate([`admin/${path}`]);
+  }
 
 }
--- src/app/admin/users/users.ts
+++ src/app/admin/users/users.ts
@@ -1,11 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
+import { AuthService } from '../../auth/auth.service';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
+  ],
   templateUrl: './users.html',
-  styleUrl: './users.css'
+  styleUrl: './users.css',
 })
-export class Users {
+export class Users implements OnInit {
+  columns = [
+    { key: 'id', label: 'ID' },
+    { key: 'nombre', label: 'Nombre' },
+    { key: 'rol', label: 'Rol' },
+  ];
 
+  displayedColumns = [...this.columns.map((col) => col.key), 'acciones'];
+
+  dataSource = [
+    // { id: '1', nombre: 'Ana García', rol: 'Admin', monto: 'S/.1000' },
+    // { id: '2', nombre: 'Juan Perez', rol: 'Cliente', monto: 'S/.1000' },
+    // { id: '3', nombre: 'Cliente3', rol: 'Cliente', monto: 'S/.1000' },
+    // { id: '4', nombre: 'Cliente4', rol: 'Cliente', monto: 'S/.1000' },
+      { id: '1', nombre: 'Ana García', rol: 'Admin', isExpanded: false },
+  { id: '2', nombre: 'Juan Perez', rol: 'Cliente', isExpanded: false },
+  { id: '3', nombre: 'Cliente3', rol: 'Cliente', isExpanded: false },
+  { id: '4', nombre: 'Cliente4', rol: 'Cliente', isExpanded: false },
+  ];
+
+  selectedUserId: string | null = null;
+  expandedElement: any | null = null;
+  showRow: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log('Hola user');
+  }
+
+  editUser(user: any) {
+    console.log(user);
+  }
+  
+  deleteUser(user: any) {
+    console.log(user);
+  }
+  
+  addUserProduct(user: any) {
+    this.showRow = !this.showRow;
+    this.selectedUserId = this.selectedUserId === user.id ? null : user.id;
+    this.expandedElement = this.expandedElement === user ? null : user;
+    console.log(user);
+    console.log(user.id);
+    console.log(this.selectedUserId);
+  }
+
+  toggleExpandedRow(user: any) {
+  this.expandedElement = this.expandedElement === user ? null : user;
+}
 }
