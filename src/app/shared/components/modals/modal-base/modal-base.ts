811935a97317d67214dc5a2dfa96e5e840e498ad
Christian Daniel Campaña Díaz <christiand0393@gmail.com>
2025-09-26 14:09:53 -0500

    modal-base-complete

--- src/app/shared/components/modals/modal-base/modal-base.ts
+++ src/app/shared/components/modals/modal-base/modal-base.ts
@@ -1,12 +1,11 @@
+import { CommonModule } from '@angular/common';
 import {
   Component,
   EventEmitter,
   Input,
-  Output,
-  ContentChild,
-  AfterContentInit,
+  Output
 } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { IButtonsInFooter } from '../../../interfaces/modal-base.interfaces';
 
 @Component({
   selector: 'app-modal-base',
@@ -15,46 +14,29 @@ import { CommonModule } from '@angular/common';
   templateUrl: './modal-base.html',
   styleUrls: ['./modal-base.css'],
 })
-export class ModalBaseComponent implements AfterContentInit {
+export class ModalBaseComponent {
   @Input() title: string = '';
-  @Input() showCloseButton: boolean = true;
-  @Input() size: 'sm' | 'md' | 'lg' | 'xl' = 'md';
-  @Input() centered: boolean = true;
-  @Input() scrollable: boolean = false;
-  @Input() animation: boolean = true;
+  @Input() withHeader?: boolean = true;
+  @Input() withFooter?: boolean = true;
+  @Input() buttonsInFooter?: IButtonsInFooter | null = null;
 
   @Output() closeModal = new EventEmitter<void>();
-  @Output() beforeClose = new EventEmitter<void>();
-  @Output() afterClose = new EventEmitter<void>();
-  @Output() afterOpen = new EventEmitter<void>();
 
   isOpen: boolean = false;
-  hasFooterContent: boolean = false;
-
-  @ContentChild('[footer]') footerContent: any;
-
-  ngAfterContentInit() {
-    this.hasFooterContent = !!this.footerContent;
-  }
 
   /**
    * Abre el modal
    */
   open(): void {
     this.isOpen = true;
-    setTimeout(() => this.afterOpen.emit(), 300);
   }
 
   /**
    * Cierra el modal
    */
   close(): void {
-    this.beforeClose.emit();
     this.isOpen = false;
-    setTimeout(() => {
-      this.closeModal.emit();
-      this.afterClose.emit();
-    }, 300);
+    this.closeModal.emit();
   }
 
   /**
